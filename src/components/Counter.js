7efe68ae326330d736afa6a66abc192d8c28ca47
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-08-25 19:51:58 -0300

    increase payload dinamic

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,70 +2,77 @@ import { Component } from 'react';
 import classes from './Counter.module.css';
 import { useSelector, useDispatch, connect } from 'react-redux';
 
-// const Counter = () => {
-//   const dispatch = useDispatch();
-//   const counter = useSelector(state => state.counter);
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
 
-//   const incrementHandler = () => {
-//     dispatch({ type: 'increment' })
-//   };
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' })
+  };
 
-//   const decrementHandler = () => {
-//     dispatch({ type: 'decrement' })
-//   };
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 })
+  };
 
-//   const toggleCounterHandler = () => { };
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' })
+  };
 
-//   return (
-//     <main className={classes.counter}>
-//       <h1>Redux Counter</h1>
-//       <div className={classes.value}>{counter}</div>
-//       <div>
-//         <button onClick={incrementHandler}>Increment</button>
-//         <button onClick={decrementHandler}>Decrement</button>
-//       </div>
-//       <button onClick={toggleCounterHandler}>Toggle Counter</button>
-//     </main>
-//   );
-// };
+  const toggleCounterHandler = () => { };
 
-class Counter extends Component {
-  incrementHandler() {
-    this.props.increment();
-  }
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increae by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
 
-  decrementHandler() {
-    this.props.decrement();
-  }
+export default Counter;
 
-  toggleCounterHandler() { }
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
 
-  render() {
-    return (
-      <main className={classes.counter}>
-        <h1>Redux Counter</h1>
-        <div className={classes.value}>{this.props.counter}</div>
-        <div>
-          <button onClick={this.incrementHandler.bind(this)}>Increment</button>
-          <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
-        </div>
-        <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
-      </main>
-    )
-  }
-}
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
 
-const mapStateToProps = state => {
-  return {
-    counter: state.counter
-  }
-}
+//   toggleCounterHandler() { }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    increment: () => dispatch({ type: 'increment' }),
-    decrement: () => dispatch({ type: 'decrement' })
-  }
-}
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     )
+//   }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// const mapStateToProps = state => {
+//   return {
+//     counter: state.counter
+//   }
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     increment: () => dispatch({ type: 'increment' }),
+//     decrement: () => dispatch({ type: 'decrement' })
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
