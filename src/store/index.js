40cbbddcf193414d7c5f1e9c0c0ddbccc77e5872
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-08-25 22:02:59 -0300

    reduxjs/toolkit createSlice inmutabilidad

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,25 @@
-import { createStore } from 'redux';
+import { createStore, createSlice } from 'redux';
 
 const initialState = { counter: 0, showCounter: true };
+
+createSlice = ({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter++;
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+})
 
 const counterReducer = (state = initialState, action) => {
     if (action.type === 'increment') {
