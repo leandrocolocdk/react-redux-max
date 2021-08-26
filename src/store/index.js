f8d36063caaf0c2b83189f403aef0c946fef952b
leandro matias suarez <leandro_colocdk@hotmail.com>
2021-08-26 14:19:44 -0300

    Redux toolkit modularizado con slice counter y auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,54 +1,13 @@
-// import { createStore } from 'redux';
-import { createSlice, configureStore } from '@reduxjs/toolkit'
-
-const initialStateCounter = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-    name: 'counter',
-    initialState: initialStateCounter,
-    reducers: {
-        increment(state) {
-            state.counter++;
-        },
-        decrement(state) {
-            state.counter--;
-        },
-        increase(state, action) {
-            state.counter = state.counter + action.payload;
-        },
-        toggleCounter(state) {
-            state.showCounter = !state.showCounter;
-        }
-    }
-})
-
-const initialAuthState = {
-    isAuthenticated: false
-}
-
-const authSlice = createSlice({
-    name: 'authentication',
-    initialState: initialAuthState,
-    reducers: {
-        login(state) {
-            state.isAuthenticated = true
-        },
-        logout(state) {
-            state.isAuthenticated = false
-        }
-    }
-})
-
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counter'
+import authReducer from './auth'
 
 const store = configureStore({
     reducer: {
-        counter: counterSlice.reducer,
-        auth: authSlice.reducer
+        counter: counterReducer,
+        auth: authReducer
     }
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
 
